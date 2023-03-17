86019a611e1c4ce5fbf9f0aad5352458d3657389
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-17 21:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function minCostToFormRope(event) {
+	event.preventDefault();
+	var inputElement = document.querySelector('input').value;
+	var arr = inputElement.split(',');
+	 arr.sort(function (a,b){return a-b});
+
+	var cost = 0;
+	while (arr.length >1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost +=res;
+		// Use Shift or splice
+		arr.shift();
+		arr.shift();
+		// arr.splice(0,2);
+		
+		arr.sort(function (a,b){return a-b});
+	}
+	document.getElementById("result").textContent = cost;
