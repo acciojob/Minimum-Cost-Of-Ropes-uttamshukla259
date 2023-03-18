3b56948933188792fa622c7b09a81b0428046ff1
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-18 19:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-function minCostToFormRope(event) {
-	event.preventDefault();
-	var inputElement = document.querySelector('input').value;
-	var arr = inputElement.split(',');
-	 arr.sort(function (a,b){return a-b});
-
+function calculateMinCost() {
+  //your code here
+var res = 0;
 	var cost = 0;
-	while (arr.length >1) {
-		var res = Number(arr[0]) + Number(arr[1]);
-		arr.push(res);
-		cost +=res;
-		// Use Shift or splice
-		arr.shift();
-		arr.shift();
-		// arr.splice(0,2);
-		
-		arr.sort(function (a,b){return a-b});
+	var inputArray = document.getElementById("rope-lengths").value;
+
+	inputArray = inputArray.split(',').map(Number);
+	console.log(inputArray);
+	for(let i = 0;i< inputArray.length -1;i+=0) {
+		if (inputArray.length == 0) break;
+		inputArray = inputArray.sort((a,b) => b-a);
+		res = inputArray.pop()+ inputArray.pop();
+		cost += res;
+		inputArray[inputArray.length] = res; }
+
+	document.getElementById("result").innerHTML = cost;
 	}
-	document.getElementById("result").textContent = cost;
